35b3acb6662caa474e48414d83343c5d340d7010
Charles Andrade <charles-j-1@hotmail.com>
2019-02-08 13:42:29 -0200

    Add project files.

---
+++ RedimensionandoImagens/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace RedimensionandoImagens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblNovoTamanhoImagem = new System.Windows.Forms.Label();
+            this.lblTamanhoOriginal = new System.Windows.Forms.Label();
+            this.btnRedimensionarImagem = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nupImagem = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestinoImagem = new System.Windows.Forms.TextBox();
+            this.txtOrigemImagem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalvarImagem = new System.Windows.Forms.Button();
+            this.btnProcurarImagem = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.picImagem);
+            this.panel1.Location = new System.Drawing.Point(8, 200);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(753, 226);
+            this.panel1.TabIndex = 1;
+            // 
+            // picImagem
+            // 
+            this.picImagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.picImagem.Location = new System.Drawing.Point(4, 3);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(746, 218);
+            this.picImagem.TabIndex = 0;
+            this.picImagem.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.lblNovoTamanhoImagem);
+            this.panel2.Controls.Add(this.lblTamanhoOriginal);
+            this.panel2.Controls.Add(this.btnRedimensionarImagem);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.nupImagem);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtDestinoImagem);
+            this.panel2.Controls.Add(this.txtOrigemImagem);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnSalvarImagem);
+            this.panel2.Controls.Add(this.btnProcurarImagem);
+            this.panel2.Location = new System.Drawing.Point(8, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(753, 181);
+            this.panel2.TabIndex = 2;
+            // 
+            // lblNovoTamanhoImagem
+            // 
+            this.lblNovoTamanhoImagem.AutoSize = true;
+            this.lblNovoTamanhoImagem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblNovoTamanhoImagem.Location = new System.Drawing.Point(310, 92);
+            this.lblNovoTamanhoImagem.Name = "lblNovoTamanhoImagem";
+            this.lblNovoTamanhoImagem.Size = new System.Drawing.Size(35, 13);
+            this.lblNovoTamanhoImagem.TabIndex = 12;
+            this.lblNovoTamanhoImagem.Text = "label6";
+            // 
+            // lblTamanhoOriginal
+            // 
+            this.lblTamanhoOriginal.AutoSize = true;
+            this.lblTamanhoOriginal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblTamanhoOriginal.Location = new System.Drawing.Point(310, 48);
+            this.lblTamanhoOriginal.Name = "lblTamanhoOriginal";
+            this.lblTamanhoOriginal.Size = new System.Drawing.Size(37, 13);
+            this.lblTamanhoOriginal.TabIndex = 11;
+            this.lblTamanhoOriginal.Text = "lablel5";
+            // 
+            // btnRedimensionarImagem
+            // 
+            this.btnRedimensionarImagem.BackColor = System.Drawing.Color.White;
+            this.btnRedimensionarImagem.Image = global::RedimensionandoImagens.Properties.Resources.tool;
+            this.btnRedimensionarImagem.Location = new System.Drawing.Point(498, 128);
+            this.btnRedimensionarImagem.Name = "btnRedimensionarImagem";
+            this.btnRedimensionarImagem.Size = new System.Drawing.Size(99, 47);
+            this.btnRedimensionarImagem.TabIndex = 10;
+            this.btnRedimensionarImagem.Text = "Redimensionar";
+            this.btnRedimensionarImagem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRedimensionarImagem.UseVisualStyleBackColor = false;
+            this.btnRedimensionarImagem.Visible = false;
+            this.btnRedimensionarImagem.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(210, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Novo Tamanho";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(210, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tamanho Original";
+            // 
+            // nupImagem
+            // 
+            this.nupImagem.Location = new System.Drawing.Point(314, 133);
+            this.nupImagem.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.nupImagem.Name = "nupImagem";
+            this.nupImagem.Size = new System.Drawing.Size(58, 20);
+            this.nupImagem.TabIndex = 7;
+            this.nupImagem.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Percentual  Largura/Altura Original";
+            this.label3.Visible = false;
+            // 
+            // txtDestinoImagem
+            // 
+            this.txtDestinoImagem.Location = new System.Drawing.Point(213, 69);
+            this.txtDestinoImagem.Name = "txtDestinoImagem";
+            this.txtDestinoImagem.Size = new System.Drawing.Size(525, 20);
+            this.txtDestinoImagem.TabIndex = 5;
+            // 
+            // txtOrigemImagem
+            // 
+            this.txtOrigemImagem.Location = new System.Drawing.Point(213, 25);
+            this.txtOrigemImagem.Name = "txtOrigemImagem";
+            this.txtOrigemImagem.Size = new System.Drawing.Size(525, 20);
+            this.txtOrigemImagem.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Imagem Destino";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Imagem Origem";
+            // 
+            // btnSalvarImagem
+            // 
+            this.btnSalvarImagem.BackColor = System.Drawing.Color.White;
+            this.btnSalvarImagem.Image = global::RedimensionandoImagens.Properties.Resources.save;
+            this.btnSalvarImagem.Location = new System.Drawing.Point(604, 127);
+            this.btnSalvarImagem.Name = "btnSalvarImagem";
+            this.btnSalvarImagem.Size = new System.Drawing.Size(99, 47);
+            this.btnSalvarImagem.TabIndex = 1;
+            this.btnSalvarImagem.Text = "Salvar Imagem";
+            this.btnSalvarImagem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSalvarImagem.UseVisualStyleBackColor = false;
+            this.btnSalvarImagem.Visible = false;
+            this.btnSalvarImagem.Click += new System.EventHandler(this.btnSalvarImagem_Click);
+            // 
+            // btnProcurarImagem
+            // 
+            this.btnProcurarImagem.BackColor = System.Drawing.Color.White;
+            this.btnProcurarImagem.Image = global::RedimensionandoImagens.Properties.Resources.find3;
+            this.btnProcurarImagem.Location = new System.Drawing.Point(10, 22);
+            this.btnProcurarImagem.Name = "btnProcurarImagem";
+            this.btnProcurarImagem.Size = new System.Drawing.Size(99, 59);
+            this.btnProcurarImagem.TabIndex = 0;
+            this.btnProcurarImagem.Text = "Procurar Imagem";
+            this.btnProcurarImagem.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnProcurarImagem.UseVisualStyleBackColor = false;
+            this.btnProcurarImagem.Click += new System.EventHandler(this.btnProcurarImagem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(767, 436);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Redimensionando Imagens";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nupImagem)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnRedimensionarImagem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nupImagem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDestinoImagem;
+        private System.Windows.Forms.TextBox txtOrigemImagem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalvarImagem;
+        private System.Windows.Forms.Button btnProcurarImagem;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Label lblTamanhoOriginal;
+        private System.Windows.Forms.Label lblNovoTamanhoImagem;
+
+    }
+}
+
---
+++ RedimensionandoImagens/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Collections;
+
+namespace RedimensionandoImagens
+{
+    public partial class Form1 : Form
+    {
+        public Size tamanhoOriginalImagem { get; set; }        //Store original image size.
+        public Size novoTamanhoImagem { get; set; }             //Store new image size.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcurarImagem_Click(object sender, EventArgs e)
+        {
+            string destinoImg = @"C:\Users\Charles\AppData\Roaming\Spark\xtra\emoticons\WhatsApp\Imagens\PNG 16px\";
+            txtDestinoImagem.Text = "";
+            lblTamanhoOriginal.Text = "";
+            lblNovoTamanhoImagem.Text = "";
+            txtOrigemImagem.Text = @"C:\Users\Charles\AppData\Roaming\Spark\xtra\emoticons\WhatsApp\Imagens\PNG Baixa";
+
+            OpenFileDialog abrirImagem = new OpenFileDialog
+            {
+                Multiselect = true
+            };
+            DialogResult resultado = abrirImagem.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                //Carrega imagem no picture box e define a propriedade size da imagem como normal
+                //picImagem.Load(abrirImagem.FileName);
+                //picImagem.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                String[] a = abrirImagem.FileNames;
+                String[] b = abrirImagem.SafeFileNames;
+                int i = 0;
+
+                foreach (var item in a)
+                {
+                    txtOrigemImagem.Text = item;
+                    txtDestinoImagem.Text = destinoImg + b[i];
+
+                    picImagem.Load(item);
+                    picImagem.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                    //Retorna a altura e lagurada da imagem e armazena na varivavel tamanhoOriginalImagem
+                    int imgWidth = picImagem.Image.Width;
+                    int imgHeight = picImagem.Image.Height;
+                    tamanhoOriginalImagem = new Size(imgWidth, imgHeight);
+
+                    string imgSize = "Width " + imgWidth + " px  Height " + imgHeight + " px";
+                    lblTamanhoOriginal.Text = imgSize;
+
+                    Image imagemEscalonada = EscalaPercentual(picImagem.Image, 23);
+                    //Exibe a imagem no picture box.
+                    picImagem.Image = imagemEscalonada;
+
+                    //Retorna a lagura e altura da imagem
+                    string imagemSize = "Width " + imagemEscalonada.Width + " px  Height " + imagemEscalonada.Height + " px";
+                    lblNovoTamanhoImagem.Text = imgSize;
+                    string nomeArquivo = txtDestinoImagem.Text;
+                    Bitmap imgImage = new Bitmap(picImagem.Image);  //Cria um objeto do tipo Bitmap
+                    imgImage.Save(nomeArquivo);      //Chama o método Save da classe Bitmap
+
+                    i++;
+
+                    //txtOrigemImagem.Text = item;
+                    //txtDestinoImagem.Text = destinoImg + abrirImagem.SafeFileNames;
+
+                    //picImagem.Load(abrirImagem.FileName);
+                    //picImagem.SizeMode = PictureBoxSizeMode.CenterImage;
+
+                    ////Retorna a altura e lagurada da imagem e armazena na varivavel tamanhoOriginalImagem
+                    //int imgWidth = picImagem.Image.Width;
+                    //int imgHeight = picImagem.Image.Height;
+                    //tamanhoOriginalImagem = new Size(imgWidth, imgHeight);
+
+                    //string imgSize = "Width " + imgWidth + " px  Height " + imgHeight + " px";
+                    //lblTamanhoOriginal.Text = imgSize;
+
+                    //Image imagemEscalonada = EscalaPercentual(picImagem.Image, 23);
+                    ////Exibe a imagem no picture box.
+                    //picImagem.Image = imagemEscalonada;
+
+                    ////Retorna a lagura e altura da imagem
+                    //string imagemSize = "Width " + imagemEscalonada.Width + " px  Height " + imagemEscalonada.Height + " px";
+                    //lblNovoTamanhoImagem.Text = imgSize;
+                    //string nomeArquivo = txtDestinoImagem.Text;
+                    //Bitmap imgImage = new Bitmap(picImagem.Image);  //Cria um objeto do tipo Bitmap
+                    //imgImage.Save(nomeArquivo);      //Chama o método Save da classe Bitmap
+                }
+                //Armazena o local da origem no textbox.
+                //txtOrigemImagem.Text = abrirImagem.FileName;
+                //txtDestinoImagem.Text = destinoImg + abrirImagem.SafeFileName;
+
+                ////Retorna a altura e lagurada da imagem e armazena na varivavel tamanhoOriginalImagem
+                //int imgWidth = picImagem.Image.Width;
+                //int imgHeight = picImagem.Image.Height;
+                //tamanhoOriginalImagem = new Size(imgWidth, imgHeight);
+
+                //string imgSize = "Width " + imgWidth + " px  Height " + imgHeight + " px";
+                //lblTamanhoOriginal.Text = imgSize;
+
+                //Image imagemEscalonada = EscalaPercentual(picImagem.Image, 23);
+                ////Exibe a imagem no picture box.
+                //picImagem.Image = imagemEscalonada;
+
+                ////Retorna a lagura e altura da imagem
+                //string imagemSize = "Width " + imagemEscalonada.Width + " px  Height " + imagemEscalonada.Height + " px";
+                //lblNovoTamanhoImagem.Text = imgSize;
+                //string nomeArquivo = txtDestinoImagem.Text;
+                //Bitmap imgImage = new Bitmap(picImagem.Image);  //Cria um objeto do tipo Bitmap
+                //imgImage.Save(nomeArquivo);      //Chama o método Save da classe Bitmap
+            }
+        }
+
+        static Image EscalaPercentual(Image imgPhoto, int Percent)
+        {
+            float nPorcentagem = ((float)Percent / 100);
+
+            int fonteLargura = imgPhoto.Width;     //armazena a largura original da imagem origem
+            int fonteAltura = imgPhoto.Height;   //armazena a altura original da imagem origem
+            int origemX = 0;        //eixo x da imagem origem
+            int origemY = 0;        //eixo y da imagem origem
+
+            int destX = 0;          //eixo x da imagem destino
+            int destY = 0;          //eixo y da imagem destino
+            //Calcula a altura e largura da imagem redimensionada
+            int destWidth = (int)(fonteLargura * nPorcentagem);
+            int destHeight = (int)(fonteAltura * nPorcentagem);
+
+            //Cria um novo objeto bitmap
+            Bitmap bmImagem = new Bitmap(destWidth, destHeight,
+                                     PixelFormat.Format24bppRgb);
+            //Define a resolução do bitmap.
+            bmImagem.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+            //Crima um objeto graphics e defina a qualidade
+            Graphics grImagem = Graphics.FromImage(bmImagem);
+            grImagem.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            //Desenha a imge usando o método DrawImage() da classe grafica
+            grImagem.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(origemX, origemY, fonteLargura, fonteAltura),
+                GraphicsUnit.Pixel);
+
+            grImagem.Dispose();  //libera o objeto grafico
+            return bmImagem;
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //Chama o método EscalaPercentual  que retorna a imagem 
+            //redimensionada e armazena-a em uma varivael do tipo Image
+            //Image imagemEscalonada = EscalaPercentual(picImagem.Image, (int)nupImagem.Value);
+            Image imagemEscalonada = EscalaPercentual(picImagem.Image, 23);
+            //Exibe a imagem no picture box.
+            picImagem.Image = imagemEscalonada;
+
+            //Retorna a lagura e altura da imagem
+            string imgSize = "Width " + imagemEscalonada.Width + " px  Height " + imagemEscalonada.Height + " px";
+            lblNovoTamanhoImagem.Text = imgSize;
+        }
+
+        private void btnSalvarImagem_Click(object sender, EventArgs e)
+        {
+            //Image imagemEscalonada = EscalaPercentual(picImagem.Image, 23);
+            ////Exibe a imagem no picture box.
+            //picImagem.Image = imagemEscalonada;
+
+            ////Retorna a lagura e altura da imagem
+            ////string imgSize = "Width " + imagemEscalonada.Width + " px  Height " + imagemEscalonada.Height + " px";
+            //lblNovoTamanhoImagem.Text = imgSize;
+            //string nomeArquivo = txtDestinoImagem.Text;
+            //Bitmap imgImage = new Bitmap(picImagem.Image);  //Cria um objeto do tipo Bitmap
+            //imgImage.Save(nomeArquivo);      //Chama o método Save da classe Bitmap
+        }
+        //private void btnSalvarImagem_Click(object sender, EventArgs e)
+        //{
+        //    SaveFileDialog sfd = new SaveFileDialog();   //Cria um objeto SaveFileDialog
+        //    sfd.DefaultExt = "png";                     //Define a extensão padrão
+        //    if (DialogResult.OK == sfd.ShowDialog())
+        //    {
+        //        string nomeArquivo = sfd.FileName;                 //Armazena o nome da arquivo na varivael
+        //        txtDestinoImagem.Text = nomeArquivo;               //Exibe o local de destino no TextBox
+        //        Bitmap imgImage = new Bitmap(picImagem.Image);  //Cria um objeto do tipo Bitmap
+        //        imgImage.Save(nomeArquivo, ImageFormat.Png);      //Chama o método Save da classe Bitmap
+        //    }
+        //}
+    }
+}
